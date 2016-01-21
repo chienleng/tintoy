83c7bc750fc0d118f2808c2bcbeaf58c6882398f
Steven Tan <steven.tan@papercut.com>
2016-01-21 14:18:35 +1100

    Jobs kanban styles update

--- client/views/operator/jobs/accepted/accepted_jobs.js
+++ client/views/operator/jobs/accepted/accepted_jobs.js
@@ -1,5 +1,9 @@
 Template.acceptedJobs.helpers({
   accepted: function() {
     return Jobs.find({status: 'accepted'}, {sort: Session.get('jobsSortOrder')});
+  },
+  count: function() {
+    return Jobs.find({status: 'accepted'}).count();
   }
+
 });
