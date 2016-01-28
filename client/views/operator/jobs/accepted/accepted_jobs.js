4a57f3eb117b679d0270ed5bca957e79d8f73d4e
Steven Tan <steven.tan@papercut.com>
2016-01-28 16:43:01 +1100

    Sort/Filter design

--- client/views/operator/jobs/accepted/accepted_jobs.js
+++ client/views/operator/jobs/accepted/accepted_jobs.js
@@ -2,8 +2,36 @@ Template.acceptedJobs.helpers({
   accepted: function() {
     return Jobs.find({'latestLog.status': 'accepted'}, {sort: Session.get('jobsSortOrder')});
   },
+  hasJobs: function() {
+    return Jobs.find({'latestLog.status': 'accepted'}).count() > 0;
+  },
   count: function() {
     return Jobs.find({'latestLog.status': 'accepted'}).count();
+  },
+  showAcceptedFilters: function() {
+    console.log(Template.instance().data.showAcceptedFilters.get())
+    return Template.instance().data.showAcceptedFilters.get();
   }
+});
+Template.acceptedJobs.events({
+  'click .accepted-jobs .current-sort a': function() {
+    var showAcceptedFilters = Template.instance().data.showAcceptedFilters.get();
+    Template.instance().data.showAcceptedFilters.set(!showAcceptedFilters);
+    return false;
+  },
+  'click .accepted-jobs .filter-form .cancel': function() {
+    Template.instance().data.showAcceptedFilters.set(false);
+    return false
+  },
+  'click .accepted-jobs .filter-form .apply': function() {
+    Template.instance().data.showAcceptedFilters.set(false);
+    return false
+  },
+  'change .accepted-jobs .sort-selection': function() {
+    Template.instance().data.showAcceptedFilters.set(false);
+  }
+});
 
+Template.acceptedJobs.onCreated(function() {
+  this.data.showAcceptedFilters = new ReactiveVar(false);
 });
--- client/views/operator/jobs/submitted/submitted_jobs.js
+++ client/views/operator/jobs/submitted/submitted_jobs.js
@@ -2,7 +2,37 @@ Template.submittedJobs.helpers({
   incoming: function() {
     return Jobs.find({'latestLog.status': 'incoming'}, {sort: Session.get('jobsSortOrder')});
   },
+  hasJobs: function() {
+    return Jobs.find({'latestLog.status': 'incoming'}).count() > 0;
+  },
   count: function() {
     return Jobs.find({'latestLog.status': 'incoming'}).count();
+  },
+  showSubmittedFilters: function() {
+    console.log(Template.instance().data.showSubmittedFilters.get())
+    return Template.instance().data.showSubmittedFilters.get();
   }
+});
+
+Template.submittedJobs.events({
+  'click .incoming-jobs .current-sort a': function() {
+    var showSubmittedFilters = Template.instance().data.showSubmittedFilters.get();
+    Template.instance().data.showSubmittedFilters.set(!showSubmittedFilters);
+    return false;
+  },
+  'click .incoming-jobs .filter-form .cancel': function() {
+    Template.instance().data.showSubmittedFilters.set(false);
+    return false
+  },
+  'click .incoming-jobs .filter-form .apply': function() {
+    Template.instance().data.showSubmittedFilters.set(false);
+    return false
+  },
+  'change .incoming-jobs .sort-selection': function() {
+    Template.instance().data.showSubmittedFilters.set(false);
+  }
+});
+
+Template.submittedJobs.onCreated(function() {
+  this.data.showSubmittedFilters = new ReactiveVar(false);
 });
